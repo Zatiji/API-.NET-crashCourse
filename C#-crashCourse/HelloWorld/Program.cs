d036a42b643b69901872c60756d481dc822507e3
David M <david.mosquera.1@ulaval.ca>
2025-01-25 00:39:28 -0500

    hello World in C# printed

---
+++ C#-crashCourse/HelloWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Console.WriteLine == print the string and ends it with '\n'
+Console.WriteLine("Hello World !");
+Console.WriteLine("Hello World !");
+
+// Console.Write == print the string but doesn't add the '\n' char
+Console.Write("First");
+Console.Write("Second");
